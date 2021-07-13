568c56e0b17c78c3a670e527a1c833bd8219ecd3
Ryan Ballantine <rgballantine@gmail.com>
2021-07-13 16:27:40 +0200

    Initial book list

---
+++ src/components/AddBook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class AddBook extends Component {
+    render() {
+        return (
+            <div>
+                Add a book
+            </div>
+        )
+    }
+}
---
+++ src/components/DisplayBook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class DisplayBook extends Component {
+    render() {
+        return (
+            <div>
+                Display a book
+            </div>
+        )
+    }
+}
